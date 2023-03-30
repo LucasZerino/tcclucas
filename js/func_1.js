11096ed110b8477717a614f4db39e2d268a1ff08
Lucas Zerino <101360532+LucasZerino@users.noreply.github.com>
2023-03-30 02:52:31 -0300

    att form funcionando
    form funcionando

--- js/func_1.js
+++ js/func_1.js
@@ -1,38 +1,5 @@
 /*  Wizard */
-jQuery(function($) {
-    "use strict";
-    $('form#wrapped').attr('action', './api/send_email_1.php');
-    $("#wizard_container").wizard({
-        stepsWrapper: "#wrapped",
-        submit: ".submit",
-        unidirectional: false,
-        beforeSelect: function(event, state) {
-            if ($('input#website').val().length != 0) {
-                return false;
-            }
-            if (!state.isMovingForward)
-                return true;
-            var inputs = $(this).wizard('state').step.find(':input');
-            return !inputs.length || !!inputs.valid();
-        }
-    }).validate({
-        errorPlacement: function(error, element) {
-            if (element.is(':radio') || element.is(':checkbox')){
-                error.insertBefore(element.next());
-            } else {
-                error.insertAfter(element);
-            }
-        }
-    });
-    //  progress bar
-    $("#progressbar").progressbar();
-    $("#wizard_container").wizard({
-        afterSelect: function(event, state) {
-            $("#progressbar").progressbar("value", state.percentComplete);
-            $("#location").text("" + state.stepsComplete + " of " + state.stepsPossible + " completed");
-        }
-    });
-});
+
 
 $("#wizard_container").wizard({
     transitions: {
